9583e846f9716ac4130c6904447bc89ac703cefa
lvx2 <94781983+lvx2@users.noreply.github.com>
2021-11-28 18:04:31 +0530

    Update Inline.py

--- LuciferMoringstar_Robot/Filter/Inline.py
+++ LuciferMoringstar_Robot/Filter/Inline.py
@@ -89,9 +89,9 @@ async def answer(bot, query):
 
 def get_reply_markup(query):
     buttons = [[
-        InlineKeyboardButton('Deploy Video', url=f'{TUTORIAL}')
+        InlineKeyboardButton('🔰 GROUP 🔰', url='https://t.me/Request_flix')
         ],[
-        InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
+        InlineKeyboardButton('🔰 CHANNEL 🔰', url='https://t.me/collection_sandm')
         ]]
     return InlineKeyboardMarkup(buttons)
 
